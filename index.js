9649500e92a54d436e0d5b1f6064f8b6ce132aaf
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-23 17:09:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 class Formatter {
   
   static capitalize(string) {
-      return string.charAt(0).toUppercase()+string.slice(1)
+      return string[0].toUppercase()+string.slice(1)
     }
 }
 
-static capitalize(string) {
-    return string[0].toUpperCase() + string.slice(1)
-  }
