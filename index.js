f91a3a2ba981d36045db8209075189e2b9f66557
Alina Kornilova <alinakornilova78@gmail.com>
2020-04-23 17:39:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,12 @@ class Formatter {
       return string.charAt(0).toUpperCase()+string.slice(1)
     }
     
-    static sanitize(string) {
-    return string.replace(/[^A-Za-z0-9\-\' ]+/g, '');
+  static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9\-\' ]+/g, '');
+  }
+  
+  static sanitize(string) {
+      return string.replace(/[^A-Za-z0-9\-\' ]+/g, '');
   }
 } 
 
